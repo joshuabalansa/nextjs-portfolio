abeafd18ebab459376a273ecd01bcc0ded53877f
joshuab <jbalansa143@gmail.com>
2025-01-07 17:03:45 +0800

    feat: footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-4 text-center p-10">
+    <footer className="dark:bg-gray-900 light:bg-white dark:text-white light:text-black py-4 text-center p-10">
       <p className="text-lg mt-2 flex items-center justify-center gap-2">
         <span>Built with</span>
-        <Link href={"https://nextjs.org/"}><SiNextdotjs title="Next.js" className="text-white inline-block hover:scale-125" /></Link>
-        <Link href={"https://tailwindcss.com/"}><RiTailwindCssFill title="Tailwind CSS" className="text-white inline-block hover:scale-125" /></Link>
+        <Link href={"https://nextjs.org/"}><SiNextdotjs title="Next.js" className="dark:text-white light:text-black inline-block hover:scale-125" /></Link>
+        <Link href={"https://tailwindcss.com/"}><RiTailwindCssFill title="Tailwind CSS" className="dark:text-white light:text-black inline-block hover:scale-125" /></Link>
       </p>
     </footer>
   );
