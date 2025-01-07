01517d0d9c78c6c487b91101ecbb4619ff350d09
joshuab <jbalansa143@gmail.com>
2025-01-07 18:13:15 +0800

    made with ♥

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,14 @@
 import { SiNextdotjs } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { FaHeart } from "react-icons/fa";
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="dark:bg-gray-900 light:bg-white dark:text-white light:text-black py-4 text-center p-10">
       <p className="text-lg mt-2 flex items-center justify-center gap-2">
-        <span>Built with</span>
-        <Link href={"https://nextjs.org/"}><SiNextdotjs title="Next.js" className="dark:text-white light:text-black inline-block hover:scale-125" /></Link>
-        <Link href={"https://tailwindcss.com/"}><RiTailwindCssFill title="Tailwind CSS" className="dark:text-white light:text-black inline-block hover:scale-125" /></Link>
+        <span>Made with </span>
+        <FaHeart /> By JoshuaB
       </p>
     </footer>
   );
