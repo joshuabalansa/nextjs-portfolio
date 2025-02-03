41c6061f967d8ff0c7349a4713c7fcde73339ace
joshuab <jbalansa143@gmail.com>
2025-02-03 09:18:16 +0800

    update

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -7,11 +7,12 @@ import web3 from "../../public/web3.png";
 import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
 import Link from "next/link";
+import { MdOutlineTimeToLeave } from "react-icons/md";
 
 const Projects = () => {
   const [activeIndex, setActiveIndex] = useState(null);
 
-  const descriptions = [
+  const title = [
     "Healthcare Management System",
     "Order Management System with Analytics",
     "Tourism Landing Page",
@@ -49,7 +50,7 @@ const Projects = () => {
   };
 
   return (
-    <section className="py-10 p-5 mt-20 dark:bg-gray-900">
+    <section className="py-10 p-3 mt-20 dark:bg-gray-900">
       <motion.h3
         className="mb-20 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center"
         initial={{ opacity: 0, y: 50 }}
@@ -59,36 +60,18 @@ const Projects = () => {
       >
        Recent Projects
       </motion.h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
         {[web1, web2, web3, web4, web5].map((web, index) => (
           <motion.div
             key={index}
             className="relative bg-white dark:bg-gray-800 rounded-3xl overflow-hidden border border-gray-100 dark:border-gray-700 group cursor-pointer"
             onClick={() => handleClick(index)}
-            whileHover={{ scale: 1.05 }}
+            whileHover={{ scale: 1.03 }}
             transition={{ duration: 0.3 }}
           >
-            {/* Image Container */}
-            <div className="relative overflow-hidden">
-              {/* <Image
-                src={web}
-                width={400}
-                height={320}
-                layout="intrinsic"
-                alt={`web${index + 1}`}
-                className="w-full h-full object-cover"
-              /> */}
-
-              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                {activeIndex === index && (
-                  <div className="text-white">{descriptions[index]}</div>
-                )}
-              </div>
-            </div>
-
             <div className="p-6">
-              <h3 className="text-xl font-medium mb-2 text-black dark:text-white">
-                {descriptions[index]}
+              <h3 className="text-sm font-medium mb-2 text-black dark:text-white">
+                {title[index]}
               </h3>
               <p className="text-gray-500 dark:text-gray-400 text-sm mb-3">
                 Tech Stack: {techStacks[index]}
